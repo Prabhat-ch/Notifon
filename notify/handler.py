798b631806d77c8f4f6c015cbbe61f903b150102
Prabhat-ch <chprabhat2@gmail.com>
2019-07-04 19:15:21 +0545

    complete working code

--- notify/handler.py
+++ notify/handler.py
@@ -1,9 +1,13 @@
 import os
-
+import requests
 
 def post_to_slack(event, context):
 
     webhook_url = os.environ['SLACK_WEBHOOK_URL']
+
+    slack_message = "{detail[Description]} - at time: {time}".format(**event)
+    data = {"text": slack_message}
+    requests.post(webhook_url, json=data)
     print(webhook_url)
     print(event)
 
